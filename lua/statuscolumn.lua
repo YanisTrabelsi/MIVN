bb1ed8a656d66733b1c1935eee1dfa48c56683af
ytrabels <ytrabels@f1r10s23.paris.42.school>
2026-06-15 05:57:40 +0200

    status col changed

--- lua/statuscolumn.lua
+++ lua/statuscolumn.lua
@@ -2,18 +2,19 @@
 local M = {}
 
 M.render = function()
-	vim.api.nvim_set_hl(0, "StatusColumnRel", { fg = "#CBA6F7" })
-	vim.api.nvim_set_hl(0, "StatusColumnRel2", { fg = "#A6F7B2" })
+	vim.api.nvim_set_hl(0, "StatusColumnPrimary", { fg = "#CBA6F7" })
+	vim.api.nvim_set_hl(0, "StatusColumnSecondary", { fg = "#A6F7B2" })
 
 	local lnum = vim.v.lnum
 	local relnum = vim.v.relnum
-	local total = vim.fn.line("$") -- nombre total de lignes du fichier
-	local width = #tostring(total) -- nombre de chiffres du plus grand numéro
+	local total = vim.fn.line("$")
+	local width = #tostring(total)
 
-	local abs = string.format("%" .. width .. "d", lnum)
 	local rel = string.format("%" .. width .. "d", relnum)
 
-	return "%#StatusColumnRel2#" .. abs .. " " .. "%#StatusColumnRel#" .. rel .. "  "
+	local color = lnum % 2 == 0 and "%#StatusColumnPrimary#" or "%#StatusColumnSecondary#"
+
+	return color .. rel .. "  "
 end
 
 return M
